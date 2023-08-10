0496c261373b749156d88d66e11fbc51b4e89cb9
Ben Carpenter <carp131@msn.com>
2023-08-10 12:22:33 -0400

    movie year

--- js/movieYear.js
+++ js/movieYear.js
@@ -1,15 +1,15 @@
 function getYear() { 
     year = document.getElementById('movieYear').value;
-    // window.location.href = '/movies/' + year + '/index.html';
-    window.location.href = '/_site/movies/' + year + '/index.html';
+    window.location.href = '/movies/' + year + '/index.html';
+    // window.location.href = '/_site/movies/' + year + '/index.html';
 }
 
 docReady(function() {
 
     const grid = document.querySelector('.grid');
     const gridItems = grid.querySelectorAll(':scope > div');
-    // const year = window.location.href.substring(35, 39);
-    const year = window.location.href.substring(33, 37);
+    // const year = window.location.href.substring(29, 33); //dev
+    const year = window.location.href.substring(33, 37); //prod
     document.getElementById('movieYear').value = year;
 
     gridItems.forEach((item, i) => {
